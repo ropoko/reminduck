d8f1723d632073ae69f89efa7fd19907f7515750
ropoko <rodrigostramantinoli@gmail.com>
2021-04-26 19:53:13 -0300

    alarm.js and reminder.js working fine

--- public/js/alarm.js
+++ public/js/alarm.js
@@ -3,53 +3,15 @@ const { ipcRenderer } = require('electron');
 const Store = require('electron-store');
 const store = new Store();
 
-// let Alarm = (title, time) => {
-//     let newElement = `
-//         <section class="rectangle alarms_created">
-//             <span onclick="location.href='/create_alarm'">
-//                 <h4>${title}</h4>
-//                     <div class="weekdays">
-//                         <a href="#">S</a>
-//                         <a href="#">M</a>
-//                         <a href="#">T</a>
-//                         <a href="#">W</a>
-//                         <a href="#">T</a>
-//                         <a href="#">F</a>
-//                         <a href="#">S</a>
-//                     </div>
-//                 <h4>${time}</h4>
-//             </span>
-//         </section>
-//     `;
-
-//     return newElement;
-// }
-
-// function getAlarm() {
-//     let column_alarms = document.getElementsByClassName('column alarms')[0];
-//     let alarms = column_alarms.getElementsByTagName('main')[0];
-    
-//     for (let i = 0; i <= store.get('LAST_ID'); i++) {
-
-//         let element = Alarm(store.get(`alarm_${i}.alarm_name`), store.get(`alarm_${i}.alarm_time`));
-
-//         let range = document.createRange();
-//         range.selectNode(alarms);
-
-//         var documentFragment = range.createContextualFragment(element);
-//         alarms.appendChild(documentFragment);
-//     }
-// }
-
 function createAlarm() {
     let alarm = {
-        id: store.get('LAST_ID') == undefined ? 0 : store.get('LAST_ID') + 1,
+        id: store.get('LAST_ID_ALARM') == undefined ? 0 : store.get('LAST_ID_ALARM') + 1,
         name: document.getElementById('alarm_name').value,
         time: document.getElementById('alarm_time').value,
         weekdays: 0
     };
 
-    ipcRenderer.send('submitForm', alarm);    
+    ipcRenderer.send('submitForm_alarm', alarm);    
 }
 
 let form = document.getElementById('create_alarm');
@@ -58,8 +20,6 @@ form.addEventListener('submit', (event) => {
     createAlarm();
 });
 
-ipcRenderer.on('get_last_id', (event, id) => {
-    console.log('last_id: ', id);
-    store.set('LAST_ID', id);
+ipcRenderer.on('get_last_id_alarm', (event, id_alarm) => {
+    store.set('LAST_ID_ALARM', id_alarm);
 });
-
