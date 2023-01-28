8c05da7994fd0c43716c6607401b8db5aa772303
ropoko <rodrigostramantinoli@gmail.com>
2023-01-28 19:28:55 -0300

    feat: rewriting in ts

---
+++ index.ts
@@ -0,0 +1,22 @@
+import { app, BrowserWindow } from 'electron'
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  win.loadFile('index.html');
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
